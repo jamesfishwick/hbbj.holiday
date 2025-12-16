3e9015e05cf067277f44ada2b9ac185f5c0c69b6
James W Fishwick <fishwick@gmail.com>
2025-12-16 11:25:38 -0500

    feat: wrap app in ErrorBoundary
    - Add ErrorBoundary wrapper to catch errors app-wide
    - Prevents entire app crash from component errors
    - Provides graceful error fallback UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { ErrorBoundary } from '@components/common';
 import { ThemeProvider } from 'next-themes';
 
 import '@assets/main.css';
@@ -7,8 +8,10 @@ import 'typeface-merriweather';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="system" attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="system" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
